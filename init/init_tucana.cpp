fea0492ecd37bc633396e906a6ffa2e4551ddfad
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-01-20 23:55:23 +0100

    Updated Redfin patch lever, fake enforcing

--- init/init_tucana.cpp
+++ init/init_tucana.cpp
@@ -56,8 +56,9 @@ void vendor_load_properties()
 {
     property_override("ro.apex.updatable", "true");
     property_override("ro.oem_unlock_supported", "0");
+    property_override("ro.boot.selinux", "enforcing");
     // fingerprint
     property_override("ro.product.model", "Mi Note 10");
-    property_override("ro.build.description", "redfin-user 11 RQ1A.201205.011 6966805 release-keys");
-    property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.011/6966805:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ1A.210105.003 7005429 release-keys");
+    property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys");
 }
