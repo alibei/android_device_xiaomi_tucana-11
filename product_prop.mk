db489ca87f113bf40eec29b80eee586e6d111658
alibei <sebastiankotzias83@gmail.com>
2021-02-02 19:48:17 +0100

    tucana: Remove duplicate
    We are using a passtrough hal

--- product_prop.mk
+++ product_prop.mk
@@ -24,7 +24,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.hwc.enable_vds \
     persist.demo.hdmirotationlock=false \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.has_HDR_display=true \
@@ -37,9 +36,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1 \
-    debug.sf.latch_unsignaled=1 \
-    debug.sf.disable_backpressure=1
+    ro.sf.blurs_are_expensive=1
 
 # Misc props
 PRODUCT_PRODUCT_PROPERTIES += \
